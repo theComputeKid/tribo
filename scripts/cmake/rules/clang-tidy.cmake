6c6d1807a49b3cdc4072099f5e863949a60f04b2
theComputeKid <thecomputekid@gmail.com>
2023-12-14 01:03:20 +0500

    Create empty project with tests Fixes #51
    Signed-off-by: theComputeKid <thecomputekid@gmail.com>

--- scripts/cmake/rules/clang-tidy.cmake
+++ scripts/cmake/rules/clang-tidy.cmake
@@ -1,4 +1,10 @@
-if(CMAKE_CXX_COMPILER_ID MATCHES "Clang")
+if(CMAKE_CXX_COMPILER_ID MATCHES "Clang" AND NOT TRIBO_CLANG_CL)
   set(CMAKE_CXX_CLANG_TIDY $<$<CONFIG:CodeCheck>:clang-tidy;--quiet>)
   set(CMAKE_C_CLANG_TIDY $<$<CONFIG:CodeCheck>:clang-tidy;--quiet>)
 endif()
+
+# * Clang-Tidy bug: https://stackoverflow.com/a/76199998
+if(TRIBO_CLANG_CL)
+  set(CMAKE_CXX_CLANG_TIDY $<$<CONFIG:CodeCheck>:clang-tidy;--quiet;--extra-args=/EHsc>)
+  set(CMAKE_C_CLANG_TIDY $<$<CONFIG:CodeCheck>:clang-tidy;--quiet>)
+endif(TRIBO_CLANG_CL)
