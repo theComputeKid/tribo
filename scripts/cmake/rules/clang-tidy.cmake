55696fcde902ade32b63779680ffdcef364fe5cb
theComputeKid <thecomputekid@gmail.com>
2023-12-14 01:08:13 +0500

    fix clang-cl
    Signed-off-by: theComputeKid <thecomputekid@gmail.com>

--- scripts/cmake/rules/clang-tidy.cmake
+++ scripts/cmake/rules/clang-tidy.cmake
@@ -5,6 +5,6 @@ endif()
 
 # * Clang-Tidy bug: https://stackoverflow.com/a/76199998
 if(TRIBO_CLANG_CL)
-  set(CMAKE_CXX_CLANG_TIDY $<$<CONFIG:CodeCheck>:clang-tidy;--quiet;--extra-args=/EHsc>)
+  set(CMAKE_CXX_CLANG_TIDY $<$<CONFIG:CodeCheck>:clang-tidy;--quiet;--extra-arg=/EHsc>)
   set(CMAKE_C_CLANG_TIDY $<$<CONFIG:CodeCheck>:clang-tidy;--quiet>)
 endif(TRIBO_CLANG_CL)
