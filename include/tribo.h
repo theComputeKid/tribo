67ee6ec290961e676dbf9fc742d5d016364009c7
theComputeKid <thecomputekid@gmail.com>
2023-12-14 23:02:46 +0500

    Enforce doxygen
    - Fixes #53
    
    Signed-off-by: theComputeKid <thecomputekid@gmail.com>

--- include/tribo.h
+++ include/tribo.h
@@ -1,31 +1,53 @@
 /**
+ * @file
  * @brief Public C interface for the tribo library.
  *
  * @copyright Copyright (c) 2023 theComputeKid
  */
 #pragma once
 #ifdef __cplusplus
+/** @brief Macro for enabling/disabling extern linkage. */
 #	define TRIBO_EXTERN_C extern "C"
 #else
+/** @brief Macro for enabling/disabling extern linkage. */
 #	define TRIBO_EXTERN_C
 #endif
 
 #ifdef _WIN32
 #	ifdef TRIBO_EXPORT_API
+/** @brief Macro for exporting symbols for Windows. */
 #		define TRIBO_LINK TRIBO_EXTERN_C __declspec(dllexport)
 #	else
+/** @brief Macro for importing symbols for Windows. */
 #		define TRIBO_LINK TRIBO_EXTERN_C __declspec(dllimport)
 #	endif
 #else
 #	ifdef TRIBO_EXPORT_API
+/** @brief Macro for exporting symbols for Unix. */
 #		define TRIBO_LINK TRIBO_EXTERN_C __attribute__((visibility("default")))
 #	else
+/** @brief Macro for importing symbols for Unix. Empty. */
 #		define TRIBO_LINK TRIBO_EXTERN_C
 #	endif
 #endif
 
+/**
+ * @brief Return the major version of the tribo project.
+ *
+ * @return the major version of the project.
+ */
 TRIBO_LINK int triboInfoVersionMajor(void);
 
+/**
+ * @brief Return the minor version of the tribo project.
+ *
+ * @return the minor version of the project.
+ */
 TRIBO_LINK int triboInfoVersionMinor(void);
 
+/**
+ * @brief Return the patch version of the tribo project.
+ *
+ * @return the patch version of the project.
+ */
 TRIBO_LINK int triboInfoVersionPatch(void);
---
+++ scripts/cmake/rules/doxygen.cmake
@@ -0,0 +1,14 @@
+# * Doxygen is used to generate API documentation, if present on the system.
+include(FindDoxygen)
+
+if(DOXYGEN_FOUND)
+  set(DOXYGEN_GENERATE_HTML YES)
+  set(DOXYGEN_GENERATE_MAN NO)
+  set(DOXYGEN_OUTPUT_DIRECTORY ${TRIBO_ROOT_DIR}/doc/build)
+  set(DOXYGEN_WARN_AS_ERROR YES)
+  set(DOXYGEN_QUIET YES)
+  doxygen_add_docs(doc
+    src test include
+    ALL
+  )
+endif(DOXYGEN_FOUND)
