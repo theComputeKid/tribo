c1048018cc90ff06dfc263b65829a71d1144853c
theComputeKid <thecomputekid@gmail.com>
2023-12-14 19:59:57 +0500

    fix linux
    Signed-off-by: theComputeKid <thecomputekid@gmail.com>

--- scripts/cmake/rules/clang-tidy.cmake
+++ scripts/cmake/rules/clang-tidy.cmake
@@ -1,10 +1,11 @@
-if(CMAKE_CXX_COMPILER_ID MATCHES "Clang" AND NOT TRIBO_CLANG_CL)
-  set(CMAKE_CXX_CLANG_TIDY $<$<CONFIG:CodeCheck>:clang-tidy;--quiet>)
-  set(CMAKE_C_CLANG_TIDY $<$<CONFIG:CodeCheck>:clang-tidy;--quiet>)
+# * Enable clang-tidy for builds with Clang.
+if(CMAKE_CXX_COMPILER_ID MATCHES "Clang")
+  if(NOT TRIBO_CLANG_CL)
+    set(CMAKE_CXX_CLANG_TIDY $<$<CONFIG:CodeCheck>:clang-tidy;--quiet>)
+    set(CMAKE_C_CLANG_TIDY $<$<CONFIG:CodeCheck>:clang-tidy;--quiet>)
+  else()
+    # * ClangCL bug with clang-tidy that does not pick up /EHsc: https://stackoverflow.com/a/76199998
+    set(CMAKE_CXX_CLANG_TIDY $<$<CONFIG:CodeCheck>:clang-tidy;--quiet;--extra-arg=/EHsc>)
+    set(CMAKE_C_CLANG_TIDY $<$<CONFIG:CodeCheck>:clang-tidy;--quiet>)
+  endif()
 endif()
-
-# * Clang-Tidy bug: https://stackoverflow.com/a/76199998
-if(TRIBO_CLANG_CL)
-  set(CMAKE_CXX_CLANG_TIDY $<$<CONFIG:CodeCheck>:clang-tidy;--quiet;--extra-arg=/EHsc>)
-  set(CMAKE_C_CLANG_TIDY $<$<CONFIG:CodeCheck>:clang-tidy;--quiet>)
-endif(TRIBO_CLANG_CL)
