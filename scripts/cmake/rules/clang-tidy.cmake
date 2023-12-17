4cc3f8df4c9489066250868e70186379cef1d0d2
theComputeKid <theComputeKid@gmail.com>
2023-12-17 22:52:06 +0500

    Fix PCH with clang-tidy (#57)

--- scripts/cmake/rules/clang-tidy.cmake
+++ scripts/cmake/rules/clang-tidy.cmake
@@ -1,10 +1,7 @@
 # * Enable clang-tidy for builds with Clang.
 if(TRIBO_CODECHECK_ENABLE_CLANGTIDY)
-  if(TRIBO_CLANG_CL OR CMAKE_CXX_COMPILER_ID MATCHES "MSVC")
-    # * ClangCL bug with clang-tidy that does not pick up /EHsc: https://stackoverflow.com/a/76199998
-    set(CMAKE_CXX_CLANG_TIDY $<$<CONFIG:CodeCheck>:clang-tidy;--quiet;--extra-arg=/EHsc>)
-    set(CMAKE_C_CLANG_TIDY $<$<CONFIG:CodeCheck>:clang-tidy;--quiet>)
-  else()
+  # * Clang-tidy does not work with PCH on windows with cmake (yet).
+  if(CMAKE_CXX_COMPILER_ID MATCHES "Clang" AND NOT TRIBO_CLANG_CL)
     set(CMAKE_CXX_CLANG_TIDY $<$<CONFIG:CodeCheck>:clang-tidy;--quiet>)
     set(CMAKE_C_CLANG_TIDY $<$<CONFIG:CodeCheck>:clang-tidy;--quiet>)
   endif()
